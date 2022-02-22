9acfb5e3b06c9cdda197c3058aa2d8228e89b23c
Volodymyr <volodymyr.sekh.dev@gmail.com>
2022-02-22 02:02:11 +0200

    add make list horizontal

--- js/main.js
+++ js/main.js
@@ -3,10 +3,17 @@ function changeStyle()
   let myH1 = document.querySelector("h1");
   myH1.classList.add("h-style");
 
-  let firstP = document.querySelector("div p");
-  firstP.classList.add("firstP-style");
+ 
+  let p = document.getElementById("myDiv");
+  p.getElementsByTagName("p")[0].style.fontWeight = "bold";
+  p.getElementsByTagName("P")[1].style.color = "red";
+  p.getElementsByTagName("p")[2].style.textDecoration = "underline";
+  p.getElementsByTagName("p")[3].style.fontStyle = "italic";
 
-  let secondP = document.querySelector("div p");
-  secondP.classList.add("secondP-style");
+  let l = document.getElementById("myList");
+  l.getElementsByTagName("li")[0].style.display = "inline";
+  l.getElementsByTagName("li")[1].style.display = "inline";
+  l.getElementsByTagName("li")[2].style.display = "inline";
+  l.remove
 }
 changeStyle();
